f36173f2de083a0b430a18f4b62f5cee71895430
indietom <tom.leo@hotmail.com>
2014-01-17 16:55:40 +0100

    commit

---
+++ spaceInvader/spaceInvader/spaceInvader/enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace spaceInvader
+{
+    class enemy:objects
+    {
+        public enemy(int x2, int y2)
+        {
+
+        }
+    }
+}

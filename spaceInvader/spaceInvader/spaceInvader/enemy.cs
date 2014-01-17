dbc4ddce456d237532629e95b1aa7076978ce24d
indietom <tom.leo@hotmail.com>
2014-01-17 20:25:55 +0100

    phouihoihi

--- spaceInvader/spaceInvader/spaceInvader/enemy.cs
+++ spaceInvader/spaceInvader/spaceInvader/enemy.cs
@@ -7,9 +7,22 @@ namespace spaceInvader
 {
     class enemy:objects
     {
+        public int speed;
+        public int countToStep;
         public enemy(int x2, int y2)
         {
 
+        }
+        public void movment(int speed2)
+        {
+            countToStep += 1;
+            if (speed2 >= 50)
+            {
+                if (countToStep >= 32)
+                {
+
+                }
+            }
         }
     }
 }
